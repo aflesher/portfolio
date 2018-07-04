cf86913bdd35f425990f963a55996613cf0f2a68
Adam Flesher <adam.flesher@gmail.com>
2018-07-04 10:08:36 -0700

    Added link to resume with social links

--- src/components/Main/Header.jsx
+++ src/components/Main/Header.jsx
@@ -6,6 +6,7 @@ import twitterLogo from "../../images/twitter-logo.png";
 import linkedinLogo from "../../images/linkedin-logo.png";
 import mailLogo from "../../images/mail-icon.png";
 import githubLogo from "../../images/github.png";
+import googleDocsLogo from "../../images/google-docs.png";
 
 class Header extends React.Component {
   render() {
@@ -28,6 +29,7 @@ class Header extends React.Component {
           <Link icon={mailLogo} text="adam.flesher@gmail.com" url="mailto:adam.flesher@gmail.com" />
           <Link icon={twitterLogo} text="@AdamFlesher" url="https://twitter.com/AdamFlesher" />
           <Link icon={githubLogo} text="/aflesher" url="https://github.com/aflesher" />
+          <Link icon={googleDocsLogo} text="Resume" url="https://docs.google.com/document/d/1eFLlwcZDcNUwEJ22zEKPnmMh5NrU80FZgJtywfC-VBo/edit#heading=h.5x0d5h95i329" />
         </div>
       </div>
     );
